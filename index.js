2daadbb218d6bafd75b215657806c2e5177f11a8
jjuanrivvera99 <juan.rivera@alegra.com>
2021-03-14 12:34:10 -0500

    parse redis url

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 'use strict';
 
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 require('dotenv').config();
 
-var redisPort = process.env.REDIS_PORT;
-var redisHost = process.env.REDIS_HOST;
-var redisPassword = process.env.REDIS_PASSWORD;
+if (process.env.REDIS_URL) {
+    const redisURL = url.parse(process.env.REDIS_URL);
+}
 
-var ioRedis = require('ioredis');
-//var redis = new ioRedis(redisPort, redisHost, redisPassword);
-var redis = new ioRedis({
+const redisPort = process.env.REDIS_PORT;
+const redisHost = process.env.REDIS_HOST;
+const redisPassword = process.env.REDIS_PASSWORD;
+
+const broadcastPort = process.env.PORT || process.env.BROADCAST_PORT || 5000;
+
+const ioRedis = require('ioredis');
+//const redis = new ioRedis(redisPort, redisHost, redisPassword);
+const redis = new ioRedis({
     port: redisPort, // Redis port
     host: redisHost, // Redis host
     password: redisPassword,
@@ -25,7 +31,6 @@ redis.on('message', function (channel, message) {
     io.emit(channel + ':' + message.event, message.data);
 });
 
-var broadcastPort = process.env.PORT || process.env.BROADCAST_PORT || 5000;
 
 server.listen(broadcastPort, function () {
     console.log('Socket server is running on port: ' + broadcastPort);
